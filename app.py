74c66812d82adc7592efa0ec3011fb340ece2d63
Kapil Singh Negi <124447041+kapilsinghnegi@users.noreply.github.com>
2024-07-04 21:42:40 +0530

    Update index.html
    Updated the structure and styles of the web application

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
 @app.route("/predict",methods=['POST'])
 def predict():
     if request.method == 'POST':
